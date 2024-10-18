e6f5c90c712365222554777119bf6aa703b47b3a
2-gmathewp <2-gmathewp@BH-R20-21.local.gfmat.org>
2024-10-18 12:56:23 +0100

    Login Page part is done after this i need to add functions to these keys

---
+++ Recipe_Application/src/Reciep_Application.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 2-gmathewp
+ */
+public class Reciep_Application {
+    public static void main(String arg){
+    
+}
+    
+}
